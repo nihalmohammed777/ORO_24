2076c0df4e5674f54b2f80c0f3d7f119f97cc7e4
nihalmohammed777 <nihalmohammedzz99@gmail.com>
2026-01-17 17:22:57 +0530

    some changes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import SignInPage from './pages/SignIn/index.jsx';
-import ServicesPage from './pages/services/index.jsx';
+import SignInPage from './pages/signin/SignIn.jsx';
+import ServicesPage from './pages/services/Services.jsx';
 
 const AppRoutes = () => {
   return (
